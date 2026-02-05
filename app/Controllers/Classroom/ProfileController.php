6f23893eca097e8cdae1ff6b32b4b1a4dca9d90f
Norberto Ramirez <info@puertosystem.com>
2026-02-05 09:44:31 -0500

    Revert dynamic data integration in profile view

--- app/Controllers/Classroom/ProfileController.php
+++ app/Controllers/Classroom/ProfileController.php
@@ -3,12 +3,14 @@ namespace App\Controllers\Classroom;
 
 class ProfileController {
     public function index($basePath, $baseUrl) {
+        $activePage = 'dashboard';
         include $basePath . '/views/layouts/web/header.php';
         include $basePath . '/views/classroom/profile.php';
         include $basePath . '/views/layouts/web/footer.php';
     }
 
     public function myCourses($basePath, $baseUrl) {
+        $activePage = 'my-courses';
         include $basePath . '/views/layouts/web/header.php';
         include $basePath . '/views/classroom/my_courses.php';
         include $basePath . '/views/layouts/web/footer.php';
